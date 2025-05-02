e160c90b7f23d44d9c1b4a49907e699fb7a9ca3a
Kirillzu86 <kirillzu86@gmail.com>
2025-05-02 13:17:05 +0500

    modified:   test1/db.sqlite3 	modified:   test1/main/__pycache__/admin.cpython-311.pyc 	modified:   test1/main/__pycache__/models.cpython-311.pyc 	modified:   test1/main/__pycache__/views.cpython-311.pyc 	modified:   test1/main/admin.py 	new file:   test1/main/migrations/0001_initial.py 	new file:   test1/main/migrations/__pycache__/0001_initial.cpython-311.pyc 	modified:   test1/main/models.py 	new file:   test1/main/static/img/logo.png 	modified:   test1/main/templates/main/base.html 	modified:   test1/main/templates/main/index/index.html 	new file:   test1/main/templates/main/product/detail.html 	new file:   test1/pproducts/2025/04/28/energy.png 	modified:   test1/test1/__pycache__/settings.cpython-311.pyc 	modified:   test1/test1/settings.py 	new file:   venv/Lib/site-packages/psycopg2-2.9.10.dist-info/INSTALLER 	new file:   venv/Lib/site-packages/psycopg2-2.9.10.dist-info/LICENSE 	new file:   venv/Lib/site-packages/psycopg2-2.9.10.dist-info/METADATA 	new file:   venv/Lib/site-packages/psycopg2-2.9.10.dist-info/RECORD 	new file:   venv/Lib/site-packages/psycopg2-2.9.10.dist-info/REQUESTED 	new file:   venv/Lib/site-packages/psycopg2-2.9.10.dist-info/WHEEL 	new file:   venv/Lib/site-packages/psycopg2-2.9.10.dist-info/top_level.txt 	new file:   venv/Lib/site-packages/psycopg2/__init__.py 	new file:   venv/Lib/site-packages/psycopg2/__pycache__/__init__.cpython-311.pyc 	new file:   venv/Lib/site-packages/psycopg2/__pycache__/_ipaddress.cpython-311.pyc 	new file:   venv/Lib/site-packages/psycopg2/__pycache__/_json.cpython-311.pyc 	new file:   venv/Lib/site-packages/psycopg2/__pycache__/_range.cpython-311.pyc 	new file:   venv/Lib/site-packages/psycopg2/__pycache__/errorcodes.cpython-311.pyc 	new file:   venv/Lib/site-packages/psycopg2/__pycache__/errors.cpython-311.pyc 	new file:   venv/Lib/site-packages/psycopg2/__pycache__/extensions.cpython-311.pyc 	new file:   venv/Lib/site-packages/psycopg2/__pycache__/extras.cpython-311.pyc 	new file:   venv/Lib/site-packages/psycopg2/__pycache__/pool.cpython-311.pyc 	new file:   venv/Lib/site-packages/psycopg2/__pycache__/sql.cpython-311.pyc 	new file:   venv/Lib/site-packages/psycopg2/__pycache__/tz.cpython-311.pyc 	new file:   venv/Lib/site-packages/psycopg2/_ipaddress.py 	new file:   venv/Lib/site-packages/psycopg2/_json.py 	new file:   venv/Lib/site-packages/psycopg2/_psycopg.cp311-win_amd64.pyd 	new file:   venv/Lib/site-packages/psycopg2/_range.py 	new file:   venv/Lib/site-packages/psycopg2/errorcodes.py 	new file:   venv/Lib/site-packages/psycopg2/errors.py 	new file:   venv/Lib/site-packages/psycopg2/extensions.py 	new file:   venv/Lib/site-packages/psycopg2/extras.py 	new file:   venv/Lib/site-packages/psycopg2/pool.py 	new file:   venv/Lib/site-packages/psycopg2/sql.py 	new file:   venv/Lib/site-packages/psycopg2/tz.py

--- test1/main/admin.py
+++ test1/main/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Category, Product
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+    list_filter = ['name']
+    search_fields = ['name']
+    list_per_page = 10
+    ordering = ['name']
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'available', 'created', 'updated', 'discount']
+    list_filter = ['available', 'created', 'updated']
+    list_editable = ['price', 'available', 'discount']
+    prepopulated_fields = {'slug': ('name',)}
+
--- test1/test1/settings.py
+++ test1/test1/settings.py
@@ -122,3 +122,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',  # или IP адрес сервера
+        'PORT': '5433',  # стандартный порт PostgreSQL
+    }
+}
